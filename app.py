e735be304257d94ce74fa4159939d3b0674dc405
jamshina <143129429+jamshina@users.noreply.github.com>
2023-11-25 06:01:03 +0000

    s

--- app.py
+++ app.py
@@ -9,17 +9,17 @@ model=pickle.load(open('iris.pkl','rb'))
 @app.route('/')
 def home():
     result=''
-    return render_template('index.html')
+    return render_template('index.html',**locals())
 
 
 @app.route('/predict',methods=['GET','POST'])
 def predict():
-    sepal_lenght=float(request.form['SL'])
-    sepal_width=float(request.form['SW'])
-    petal_lenght=float(request.form['PL'])
-    petal_width=float(request.form['PW'])
-    result = model.predict([[sepal_lenght,sepal_width,petal_lenght,petal_width]])
-    return render_template('index.html')
+    SL=float(request.form['SL'])
+    SW=float(request.form['SW'])
+    PL=float(request.form['PL'])
+    PW=float(request.form['PW'])
+    result = model.predict([[SL,SW,PL,PW]])
+    return render_template('index.html',**locals())
 
 if __name__=='__main__':
     app.run(debug=True)         
